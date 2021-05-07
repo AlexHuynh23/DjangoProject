c6592141cde8ef5ac61976fa29713ad6ccd88f21
AlexHuynh23 <70555816+AlexHuynh23@users.noreply.github.com>
2021-05-07 11:16:33 -0700

    Added Following to admin and created super user.

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,7 +5,10 @@ from django.contrib import admin
 from .models import Profile
 from .models import Post
 from .models import Comment
+from .models import Following
+
 
 admin.site.register(Profile)
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Following)
